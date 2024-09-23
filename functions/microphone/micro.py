fc050dda27d3a245123fd912a41336e3c4e0667d
tranquocvinh9999 <tranquocvinh9999s@gmail.com>
2024-09-23 11:11:37 +0700

    time out micro

--- functions/microphone/micro.py
+++ functions/microphone/micro.py
@@ -9,10 +9,12 @@ def recognize_speech():
     recognizer.energy_threshold = 300 
 
     with sr.Microphone() as source:
-        speak("Xin vui lòng nói...")
-        recognizer.adjust_for_ambient_noise(source) 
-        audio = recognizer.listen(source, timeout=3, phrase_time_limit=5) 
-
+        try:
+            audio = recognizer.listen(source, timeout=3, phrase_time_limit=5)
+        except sr.WaitTimeoutError:
+            speak("Xin vui lòng nói...")
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source, phrase_time_limit=5)
         try:
             text = recognizer.recognize_google(audio, language='vi-VN')
             speak(f"Bạn đã nói: {text}")
@@ -23,6 +25,5 @@ def recognize_speech():
         except sr.RequestError as e:
             speak(f"Không thể kết nối đến dịch vụ nhận dạng: {e}")
             return None
-
 if __name__ == "__main__":
     recognized_text = recognize_speech()
