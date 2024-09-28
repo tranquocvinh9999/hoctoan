78a23e83e5ee09c2d7caf97cbb2ec8af77e646ad
tranquocvinh9999 <tranquocvinh9999s@gmail.com>
2024-09-28 22:50:22 +0700

    nâng cấp ai có cảm xúc và lưu câu hỏi ( xem cái này )

--- AI/bot.py
+++ AI/bot.py
@@ -64,7 +64,8 @@ def check_question_AI(question, answer, user_answer):
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(f"""giả sử bạn là một giáo viên dạy toán. Đây là bài toán tôi đưa ra: Hãy giải bài toán: {question}. Kết quả học sinh gửi tôi là
  {answer} và kết quả đúng của câu hỏi là {answer}. Bạn hay kiểm tra nếu kết quả này là đúng hay sai. Nếu đúng không tạo câu trả lời mà chỉ in ra đúng, sai thì trả về các bước để khắc phúc nhưng không được nêu ra đáp án bạn nhắc nhở rằng bạn đang nói với học sinh của bạn
-bằng cách xưng em chi tiết hơn nhé và cảm xúc như thầy trò nhé""")
+bằng cách xưng em chi tiết hơn nhé và cảm xúc như thầy trò nhé và đặc biệt phải nhớ rằng bạn hãy nói có cảm xúc như thầy trò nhé vậy học sinh của bạn liệt kê các câu trả lời sai trong câu trả lời đó thì bạn sẽ nói sao với trường hợp đó
+ĐẶC BIỆT NHỚ LÀ CHỈ RÕ ra các câu trả lời sai đó và nói tại sao""")
 def get_data_from_given_question_and_ask_AI(name):
     print(name)
     filepath = f"question_folder/{name}/questions.json"
